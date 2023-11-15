8fd02c44658e807d3441c909fdfd5228f0bc1188
Max <55560490+maks1302@users.noreply.github.com>
2023-11-15 17:42:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # API bot token from bot father https://t.me/botfather
 
 
-BOT_TOKEN = '1219711391:AAFyxQGOJpHlohcOxB_3Ed7Ie_dVUiswiH0'
+BOT_TOKEN = 'Bot-token'
 
 districts = ['Burglesum', 'Borgfeld', 'Blumenthal', 'Blockland', 'Finndorff', 'Gröpelingen',
              'Hemelingen', 'Horn-Lehe', 'Huchting', 'Neustadt', 'Bremen (whole city)']
